3f6adf5fd1bcc6e0e40121c8825b418770e458cd
shinset <50031881+shinset@users.noreply.github.com>
2020-07-24 09:06:34 +0200

    refactoring all console message
    refactoring console message for readability
    https://areknawo.com/getting-creative-with-the-console-api/

--- cmplog.js
+++ cmplog.js
@@ -8,12 +8,12 @@ try { // lets try to call a CMP framework
         
    /* Then PING the __tcfapi to check the CMP load & status then return the full object */     
   __tcfapi('ping', 2, (pingReturn) => {
-console.log('Ping the cmp V2 infos \n cmpLoaded is = '+ pingReturn.cmpLoaded +' \n cmpStatus is = ' + pingReturn.cmpStatus);
-console.log(pingReturn);
+console.info('Ping the cmp V2 infos \n cmpLoaded is = '+ pingReturn.cmpLoaded +' \n cmpStatus is = ' + pingReturn.cmpStatus);
+console.debug(pingReturn);
 
 /*Display a warning IF the cmploaded return FALSE */
   if (pingReturn.cmpLoaded === false ){ 
-console.log(" !!! WARNING !!! __tcfapi found (CMP v2) but not loaded ! check the CMP \n " + "cmpLoaded = " + pingReturn.cmpLoaded )
+console.warn(" !!! WARNING !!! __tcfapi found (CMP v2) but not loaded ! check the CMP \n " + "cmpLoaded = " + pingReturn.cmpLoaded )
 alert(" !!! WARNING !!! __tcfapi (CMP V2) found but not loaded ! check the CMP \n " + "cmpLoaded = " + pingReturn.cmpLoaded )
 };
  
@@ -27,14 +27,14 @@ alert(" !!! WARNING !!! __tcfapi (CMP V2) found but not loaded ! check the CMP \
  which mean the cmp is loaded but misconfigured or the user declined the whole CMP  */
   
             if (cmp_V2.cmpStatus == 'loaded' && (cmp_V2.tcString == '' || cmp_V2.tcString == null)) {
-                console.log('CONSENT STRING MISSING CHECK USER ACTIONS in cmp UI or CMP integration')
+                console.warn('CONSENT STRING MISSING CHECK USER ACTIONS in cmp UI or CMP integration')
             }
 
  /* IF cmpStatus is loaded & tcString is not empty or null ,then proceed and log the infos*/           
             if (cmp_V2.cmpStatus == 'loaded' && (cmp_V2.tcString != '' || cmp_V2.tcString != null)) {
-                console.log("FOUND TCFAPI CMP.V2\nConsent string V2 (C)\n Copy & Paste it into an IAB TCF V2 decoder like https://consentstringdecoder.com or https://iabtcf.com/#/decode \n Vendors list https://iabeurope.eu/vendor-list-tcf-v2-0/ \n json version https://vendorlist.consensu.org/v2/vendor-list.json ");
-                console.log(cmp_V2.tcString); // log the console string alone for ease of use and try to copy later
-                console.log(cmp_V2); // log the whole object for more detail 
+                console.info("FOUND TCFAPI CMP.V2\nConsent string V2 (C)\n Copy & Paste it into an IAB TCF V2 decoder like https://consentstringdecoder.com or https://iabtcf.com/#/decode \n Vendors list https://iabeurope.eu/vendor-list-tcf-v2-0/ \n json version https://vendorlist.consensu.org/v2/vendor-list.json ");
+                console.info(cmp_V2.tcString); // log the console string alone for ease of use and try to copy later
+                console.debug(cmp_V2); // log the whole object for more detail 
               copy(cmp_V2.tcString); // try to copy the consent string to clipboard ( it may fail sometime depending the site)
               alert("Consent string V2 copied to clipboard ! ");
 
@@ -53,14 +53,14 @@ alert(" !!! WARNING !!! __tcfapi (CMP V2) found but not loaded ! check the CMP \
       /* Check IF the consent string (consentData) is empty or null which mean integration issue or user decline the whole cmp */
         __cmp('getConsentData', 1, cmp_V1 => {
             if (cmp_V1.consentData == '' || cmp_V1.consentData == null) {
-                console.log('CONSENT STRING MISSING CHECK USER ACTIONS in cmp UI or CMP integration')
+                console.warn('CONSENT STRING MISSING CHECK USER ACTIONS in cmp UI or CMP integration')
             }
 
             /* if consentData is not empty or null then proceed the data log */
             if (cmp_V1.consentData != '' || cmp_V1.consentData != null) {
-                console.log("FOUND CMP.V1\nConsent string V1 (B)\n Copy & Paste it into an IAB TCF V1  decoder like https://acdn.origin.appnexus.net/cmp/docs/#/tools/vendor-cookie-inspector or use cookie glass https://chrome.google.com/webstore/detail/cookie-glasses/gncnjghkclkhpkfhghcbobednpchjifk");
-                console.log(cmp_V1.consentData); // log the consent string alone for ease of use 
-                console.log(cmp_V1); // log the whole object for more detail 
+                console.info("FOUND CMP.V1\nConsent string V1 (B)\n Copy & Paste it into an IAB TCF V1  decoder like https://acdn.origin.appnexus.net/cmp/docs/#/tools/vendor-cookie-inspector or use cookie glass https://chrome.google.com/webstore/detail/cookie-glasses/gncnjghkclkhpkfhghcbobednpchjifk");
+                console.info(cmp_V1.consentData); // log the consent string alone for ease of use 
+                console.debug(cmp_V1); // log the whole object for more detail 
                                 copy(cmp_V1.consentData); // try to copy the consent string
                                 alert("Consent string V1 copied to clipboard ! ");
 
@@ -79,8 +79,8 @@ not much detail here was I dont have use case and its returned in the  __tcfapi 
 */
         if (typeof(__uspapi) != 'undefined') {
 __uspapi('getUSPData', 1, (CCPA) =>{
-                console.log('FOUND CCPA FRAMEWORK \n ')
-                console.log(CCPA)
+                console.info('FOUND CCPA FRAMEWORK \n ')
+                console.debug(CCPA)
             }
 
     )
@@ -92,7 +92,7 @@ __uspapi('getUSPData', 1, (CCPA) =>{
 /*  IF all the cmps invocation returned undefined that mean either CMP is not iab compliant OR  missing on the site */
 
     if (typeof(__cmp) == 'undefined' && typeof(__tcfapi) == 'undefined' && typeof(__uspapi)== 'undefined') {
-        console.log(" !! NO IAB FRAMEWORK FOUND !! \n Check if there is GDPR string in your adcall and contact publisher")
+        console.error(" !! NO IAB FRAMEWORK FOUND !! \n Check if there is GDPR string in your adcall and contact publisher")
                 alert(" !! NO IAB FRAMEWORK FOUND !! \n Check if there is GDPR string in your adcall and contact publisher")
 
     }

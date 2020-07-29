b3f032085cc731d931ac18550eaf6cb44a5f972f
shinset <50031881+shinset@users.noreply.github.com>
2020-07-29 09:15:08 +0200

    cleaning
    remove / comment copy()
    remove bye msg

--- cmplog.js
+++ cmplog.js
@@ -54,6 +54,8 @@ alert(" !!! WARNING !!! __tcfapi (CMP V2) found but not loaded ! check the CMP \
 
                     if (cmp_V2.vendor.consents[755] === true) { // check the value of consent index 755 (google)
                         console.log("YES its = " + cmp_V2.vendor.consents[755]);
+                           console.info("consents vendors table");
+                        console.table(cmp_V2.vendor.consents);
                     } else {
                         console.warn("NO! Please check the GVL version >= [48] & the vendors table below as it return index 755 = " + cmp_V2.vendor.consents[755]);
                         //log the full vendors list  
@@ -143,7 +145,7 @@ catch (e) {
     console.error(e);
 }
 
- console.log('%c + B Y E +', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
+ // console.log('%c + B Y E +', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
 
 } 
 // call my useless function  now 
